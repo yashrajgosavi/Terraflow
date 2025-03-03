3dc1dd7a06a65e9bb18967e06eec415ff56e8c90
Yashraj Gosavi <yashrajgosavi2580@gmail.com>
2025-03-03 16:49:56 +0000

    fix: updated outputs

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -1,7 +1,23 @@
-output "output-id" {
-  value = kubernetes_namespace.namespace-test.id
+output "output-argocd-id" {
+  value = kubernetes_namespace.argocd.id
 }
 
-output "output-namespace-name" {
-  value = kubernetes_namespace.namespace-test.metadata
+output "output-namespace-argocd-name" {
+  value = kubernetes_namespace.argocd.metadata[0].name
+}
+
+output "output-prod-id" {
+  value = kubernetes_namespace.prod.id
+}
+
+output "output-namespace-prod-name" {
+  value = kubernetes_namespace.prod.metadata[0].name
+}
+
+output "output-test-id" {
+  value = kubernetes_namespace.test.id
+}
+
+output "output-namespace-test-name" {
+  value = kubernetes_namespace.test.metadata[0].name
 }
