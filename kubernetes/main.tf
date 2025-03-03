67bb89d81a4b6e4e54f22f4acab520b29e9041d4
Yashraj Gosavi <yashrajgosavi2580@gmail.com>
2025-03-03 16:32:02 +0000

    fix: added output example

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -7,6 +7,14 @@ terraform {
 }
 
 provider "kubernetes" {
-  config_path = "~/.kube/config"  # Path to your kubeconfig file
+  config_path    = "~/.kube/config" # Path to your kubeconfig file
   config_context = "minikube"
+}
+
+output "output-id" {
+  value = kubernetes_namespace.namespace-test.id
+}
+
+output "output-namespace-name" {
+  value = kubernetes_namespace.namespace-test.metadata
 }
