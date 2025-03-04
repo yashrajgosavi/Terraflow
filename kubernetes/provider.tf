d54c435c9e93705509d3cac4b00b71813033b739
Yashraj Gosavi <yashrajgosavi2580@gmail.com>
2025-03-04 17:03:50 +0000

    fix: updated the cretifiates to base64 decode

--- kubernetes/provider.tf
+++ kubernetes/provider.tf
@@ -14,24 +14,24 @@ terraform {
 
 provider "kubernetes" {
   host                   = var.k8s_host
-  client_certificate     = var.k8s_client_certificate
-  client_key             = var.k8s_client_key
-  cluster_ca_certificate = var.k8s_cluster_ca_certificate
+  client_certificate     = base64decode(var.k8s_client_certificate)
+  client_key             = base64decode(var.k8s_client_key)
+  cluster_ca_certificate = base64decode(var.k8s_cluster_ca_certificate)
 }
 
 provider "kubectl" {
   host                   = var.k8s_host
-  client_certificate     = var.k8s_client_certificate
-  client_key             = var.k8s_client_key
-  cluster_ca_certificate = var.k8s_cluster_ca_certificate
-  load_config_file = false
+  client_certificate     = base64decode(var.k8s_client_certificate)
+  client_key             = base64decode(var.k8s_client_key)
+  cluster_ca_certificate = base64decode(var.k8s_cluster_ca_certificate)
+  load_config_file       = false
 }
 
 provider "helm" {
   kubernetes {
     host                   = var.k8s_host
-    client_certificate     = var.k8s_client_certificate
-    client_key             = var.k8s_client_key
-    cluster_ca_certificate = var.k8s_cluster_ca_certificate
+    client_certificate     = base64decode(var.k8s_client_certificate)
+    client_key             = base64decode(var.k8s_client_key)
+    cluster_ca_certificate = base64decode(var.k8s_cluster_ca_certificate)
   }
 }
--- kubernetes/variables.tf
+++ kubernetes/variables.tf
@@ -23,3 +23,12 @@ variable "k8s_cluster_ca_certificate" {
   description = "kubectl config view --minify --raw -o jsonpath='{.clusters[0].cluster.certificate-authority}'"
   sensitive   = true
 }
+
+# kubectl config view --minify --raw -o jsonpath='{.clusters[0].cluster.server}{"\n"}{.users[0].user.client-certificate}{"\n"}{.users[0].user.client-key}{"\n"}{.clusters[0].cluster.certificate-authority}{"\n"}'
+
+# {
+#   echo "host: $(kubectl config view --minify -o jsonpath='{.clusters[0].cluster.server}')";
+#   echo "cluster_ca_certificate: $(cat ~/.minikube/ca.crt | base64 -w 0)";
+#   echo "client_certificate: $(cat ~/.minikube/profiles/minikube/client.crt | base64 -w 0)";
+#   echo "client_key: $(cat ~/.minikube/profiles/minikube/client.key | base64 -w 0)";
+# } > minikube_credentials.txt
