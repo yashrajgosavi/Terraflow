7a79f56c753d71f3c6bf79b17aa9480a6e2e9aae
yashrajgosavi <yashrajgosavi2580@gmail.com>
2025-03-08 21:29:26 +0530

    feat: updated application set

--- kubernetes/argocd-application-set.tf
+++ kubernetes/argocd-application-set.tf
@@ -1,17 +1,44 @@
-resource "helm_release" "application_set_test" {
-  name       = "product-test"
-  chart      = "ApplicationSet"
-  namespace  = kubernetes_namespace.argocd.metadata.0.name
-  timeout    = "1200"
-  values     = [templatefile("./configs/ApplicationSet/product-test.yaml", {})]
-  provider   = helm
+resource "kubectl_manifest" "application_set_test" {
+  yaml_body = <<EOT
+  apiVersion: argoproj.io/v1alpha1
+  kind: ApplicationSet
+  metadata:
+    name: product-backend-test
+    namespace: argocd
+  spec:
+    generators:
+    - git:
+        repoURL: https://github.com/yashrajgosavi/argo-cd-helm-charts
+        revision: main
+        directories:
+        - path: apps/*
+        - exclude: true      
+          path: apps/backup/
+    template:
+      metadata:
+        name: '{{path.basename}}-test'
+      spec:
+        project: product-test
+        syncPolicy:
+          automated:
+            prune: true
+            selfHeal: true
+        source:
+          helm:
+            valueFiles:
+              - values-test.yaml
+          repoURL: https://github.com/yashrajgosavi/argo-cd-helm-charts
+          targetRevision: main
+          path: '{{path}}'
+        destination:
+          name: in-cluster
+          server: https://kubernetes.default.svc
+          namespace: product-test
+  EOT
+  depends_on = [ argocd_project.test ]
 }
 
-# resource "helm_release" "application_set_prod" {
-#   name       = "product-prod"
-#   chart      = "ApplicationSet"
-#   namespace  = kubernetes_namespace.argocd.metadata.0.name
-#   timeout    = "1200"
-#   values     = [templatefile("./configs/ApplicationSet/product-prod.yaml", {})]
-#   provider   = helm
+# resource "argocd_application_set" "application_set_prod" {
+#   yaml_body = [templatefile("./configs/ApplicationSet/product-prod.yaml", {})]
+#   depends_on = [ argocd_project.prod ]
 # }
