d411ffced6c1a1622345e74a9243e56350961b17
yashrajgosavi <yashrajgosavi2580@gmail.com>
2025-03-08 15:10:46 +0530

    fix: updated file path

--- kubernetes/argocd-application-set.tf
+++ kubernetes/argocd-application-set.tf
@@ -3,15 +3,15 @@ resource "helm_release" "application_set_test" {
   chart      = "ApplicationSet"
   namespace  = kubernetes_namespace.argocd.metadata.0.name
   timeout    = "1200"
-  values     = [templatefile("./argocd/ApplicationSet/product-test.yaml", {})]
+  values     = [templatefile("./configs/ApplicationSet/product-test.yaml", {})]
   provider   = helm
 }
 
-resource "helm_release" "application_set_prod" {
-  name       = "product-prod"
-  chart      = "ApplicationSet"
-  namespace  = kubernetes_namespace.argocd.metadata.0.name
-  timeout    = "1200"
-  values     = [templatefile("./argocd/ApplicationSet/product-prod.yaml", {})]
-  provider   = helm
-}
+# resource "helm_release" "application_set_prod" {
+#   name       = "product-prod"
+#   chart      = "ApplicationSet"
+#   namespace  = kubernetes_namespace.argocd.metadata.0.name
+#   timeout    = "1200"
+#   values     = [templatefile("./configs/ApplicationSet/product-prod.yaml", {})]
+#   provider   = helm
+# }
